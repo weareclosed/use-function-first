e39bc2d68e422b9f6cbf70037d450d6fe5e512fa
Alex Shvets <dev@sashok.mk.ua>
2014-06-12 23:09:29 +0300

    version 0.0.1

--- aqueduct.js
+++ aqueduct.js
@@ -1,79 +1,118 @@
-module.exports = pipe;
+module.exports = use;
 
-var curry = require("curry");
+use.curry = curry;
+use.callbackFirst = callbackFirst;
 
-/** Compose, curry and reverse arguments in passed as array (or space-separated string) functions from some object(s). */
-function pipe(/** function|string|Array */ what, /** ...object */ fromObject){
-    var whatFn, whatArray;
-    var fromObjects = Array.prototype.slice.apply(arguments, 1);
+/** Compose, curry and pop up last argument in passed as array (or space-separated string) functions from some object(s). */
+function use(/** function|string|Array */ what, /** ...object */ fromObject){
+    var useFn, useNames;
+    var fromObjects = Array.prototype.slice.call(arguments, 1);
 
     if( typeof what === 'function' ){
-        whatFn = what;
-        whatArray = getArgNames( whatFn );
+        useFn = what;
+        useNames = getArgNames( useFn );
 
     }else if( what instanceof Array ){
-        whatArray = what;
+        useNames = validateNames( what );
 
     }else if( typeof what === 'string' ){
-        whatArray = what.split( /\s+/ );
+        useNames = validateNames( what.split( /\s+/ ) );
+
+    }else throw new TypeError;
+
+    var adaptedFns = adaptMethods( useNames, fromObjects );
+
+    if(!useFn ){
+        useFn = composeMethods( useNames, adaptedFns );
     }
 
-    var adaptedFns = adaptFns( whatArray, fromObjects );
+    return function composed(){
+        return useFn.apply(this, adaptedFns);
+    }
+}
 
-    if(!whatFn ){
-        whatFn = makeFnFromMethodNames( whatArray, adaptedFns );
+    function validateNames( names ){
+        //todo: names.filter
     }
 
-    return curry( function(){
-        return whatFn.apply(this, adaptedFns);
-    } );
-}
+    function composeMethods( names, object ){
+        //todo: return Function( "return " + names.join("(")+ );
+    }
 
-function makeFnFromMethodNames( methodNames, lib ){
+    function adaptMethods( names, objects ){
+        return names.map(function( name ){
+            return callbackFirst( findMethod(name, objects) );
+        });
+    }
+        function findMethod(name, objects){
+            return objects.find( methodByName );
 
-
-}
-
-function adaptFns( whatArray, fromObjects ){
-
-    var adaptedFns = new Array( whatArray.lenght );
-
-    whatArray.forEach(function( name, i ){
-        var fn = find(name, fromObjects);
-        if( fn ){
-            adaptedFns[ i ] = adapt( fn );
+            function methodByName( obj ){
+                var prop = obj[ name ];
+                if(typeof prop === 'function'){
+                    prop = prop.prototype;
+                }
+                return prop;
+            }
         }
-    });
 
-    return adaptedFns;
-}
-
-function find(id, objects){
-    return objects.any(function(obj){
-        var fn = obj[ id ];
-        if(typeof fn === 'function'){
-            fn = fn.protoype;
+        /** Move last argument to the first place */
+        function callbackFirst( fn ){
+            switch( fn.length ){
+                case 0: return fn;
+                case 1: return curry(fn);
+                case 2: return curry(function(a,b){
+                    return fn.call(this,      b,a);
+                });
+                case 3: return curry(function(a,b,c){
+                    return fn.call(this,      c,a,b);
+                });
+                default:return curry(function(){
+                    var last = arguments.length -1;
+                    var args = Array.prototype.slice.call(arguments, 0, last -1);
+                    args.unshift(arguments[last]);
+                    return fn.apply(this, args);
+                });
+            }
         }
-        return fn;
-    });
-}
 
-function adapt( fn ){
-    return curry.adapt( fn );
-}
+        /**
+         * Curry a function
+         * @copyright https://github.com/ForbesLindesay/curry/blob/1.0.0/index.js#L12
+         *
+         * @param  {function} fn The function you wish to curry
+         * @return {function}
+         */
+        function curry( fn ){
+            return function curried(){
+                if (arguments.length < fn.length) {
+                    var args = makeArray(arguments);
+                    return function () {
+                        return curried.apply(this, args.concat(makeArray(arguments)));
+                    }
+                } else {
+                    return fn.apply(this, arguments);
+                }
+            };
+        }
 
-/**
- * Parses a function and returns an array of its parameters
- * @param {function()} fn
- * @return {Array.<string>}
- * (C) https://github.com/cmtt/dijs/blob/v0.0.2/lib/di.js#L20
- */
-function getArgNames(fn) {
-    var fnRgx = /function(\s*)(\w*)[^(]*\(([^)]*)\)/m;
-    var s = fn.toString();
-    var matches = fnRgx.exec(s);
-    var params = [];
-    if (!matches) return null;
-    if (matches[3].length) params = matches[3].split(/\s*,\s*/);
-    return params;
-}
+            function makeArray(args){
+                return Array.prototype.slice.apply(args);
+            }
+
+    /**
+     * Parses a function and returns an array of its parameters
+     * @copyright https://github.com/cmtt/dijs/blob/v0.0.2/lib/di.js#L20
+     *
+     * @param {function()} fn
+     * @return {Array.<string>}
+     */
+    function getArgNames(fn) {
+        var fnRgx = /function(\s*)(\w*)[^(]*\(([^)]*)\)/m;
+        var s = fn.toString();
+        var matches = fnRgx.exec(s);
+        var params = [];
+        if (!matches) return null;
+        if (matches[3].length) params = matches[3].split(/\s*,\s*/);
+        return params;
+    }
